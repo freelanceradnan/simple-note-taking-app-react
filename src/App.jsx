bb887e0195164869b455b6e7a0d0adc875d1e672
freelanceradnan <reactorbro722@gmail.com>
2025-10-27 00:07:35 +0600

    delete button in the app included

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
+import { use, useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
 
-function App() {
-const [notesTitle,setNotesTitle]=useState("")
+function App(){
+const [noteTitle,setNoteTitle]=useState("")
 const [notes,setNotes]=useState([
   {id:1,title:'adnan'},
   {id:1,title:'adnan'}
@@ -13,44 +13,46 @@ const [editMode,setEditMode]=useState(false)
 const [editableNote,setEditableNote]=useState(null)
 
 const changeHandler=(e)=>{
- setNotesTitle(e.target.value)
+  setNoteTitle(e.target.value)
 }
 const submitHandler=(e)=>{
- e.preventDefault();
- if(notesTitle.trim() ===""){
-  return alert(`please enter a valid data`)
- }
- const newNote={
+  e.preventDefault();
+  if(noteTitle.trim()===""){
+  return alert(`please enter a vaild input value`)
+  }
+  const newNote={
   id:Date.now()+"",
-  title:notesTitle,
- };
- setNotes([...notes,newNote])
- setNotesTitle("")
+  title:noteTitle,
+  }
+  setNotes([newNote,...notes])
+}
+const removeHandler=(noteId)=>{
+  const updateNotes=notes.filter((item)=>item.id!==noteId)
+  setNotes(updateNotes)
 }
   return(
-    <div className="Form-Body">
-    <form onSubmit={submitHandler}>
-      <input type="text" value={notesTitle} onChange={changeHandler}/>
-      <button type="submit">Add Note</button>
-    </form>
-    <div className="form-list">
-<h2>Add Data Now</h2>
-<ul>
-  {notes.map((note)=>(
-    <>
-    <li>
-      <span>{note.title}</span>
-      <button>Edit</button>
-      <button>Delete</button>
-    </li>
-    </>
-  ))}
-</ul>
-    </div>
-    </div>
-  )
-   
+    
+ <div className="form-Body">
+  <h2>All Notes</h2>
+  <form onSubmit={submitHandler}>
+    <input type="text" value={noteTitle} onChange={changeHandler}/>
+    <button type="submit">Add</button>
+  </form>
   
+  <ul className="notes">
+    {notes.map((note)=>(
+      <>
+      <li>
+        <span>{note.title}</span>
+        <button>Edit</button>
+        <button onClick={()=>removeHandler(note.id)}>Delete</button>
+        </li>
+      </>
+      
+    ))}
+  </ul>
+ </div>
+  )
 }
 
 export default App
